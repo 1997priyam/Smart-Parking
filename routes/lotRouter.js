9fbfa9bcd6365bbcd0467244261552c11370119e
1997priyam <1997priyam@gmail.com>
2020-07-14 12:36:19 +0530

    lot router modified and completed

--- routes/lotRouter.js
+++ routes/lotRouter.js
@@ -6,24 +6,32 @@ router.get('/', function(req, res) {
     query = 'SELECT * from ParkingLots';
     pool.query(query, (err, results) => {
         if (err) {
-            res.status(500).json({error: true});
+            return res.status(500).json({error: true});
         }
         console.log(results);
-        res.status(200).json(results.rows);
+        return res.status(200).json(results.rows);
     })
 });
 
 router.get('/:lot_name', function(req, res) {
-    let { lot_name } = req.params
-    console.log(lot_name);
-    query = `SELECT * from ParkingLots where lotname='${lot_name}'`;
+    let { lot_name } = req.params;
+    let parkingLotInfo;
+    let query = `SELECT * from ParkingLots where lotname='${lot_name}'`;
     pool.query(query, (err, results) => {
-        if (err) {
-            res.status(500).json({error: true});
-        }
-        console.log(results);
-        res.status(200).json(results.rows);
-    })
+        if (err) return res.status(500).json({error: true});
+        if(results.rowCount > 1) return res.status(500).json({error: 'Duplicate parking lots found!!'});
+        else if(results.rowCount === 0) return res.status(400).json({error: `Parking lot with name ${lot_name} not found !`});
+        parkingLotInfo = results.rows[0];
+
+        let queryInner = `SELECT * from Bays where lotname='${lot_name}'`;
+        pool.query(queryInner, (err, results) => {
+            if (err) return res.status(500).json({error: true});
+            parkingLotInfo.bays = results.rows;
+            return res.status(200).json(parkingLotInfo);
+            })
+        });
+
+    
 });
 
 module.exports = router;
