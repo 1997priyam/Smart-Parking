8a8111fb8cb0bbf59f3623b8ff5525b0765e5816
1997priyam <1997priyam@gmail.com>
2020-07-14 13:13:42 +0530

    data router is completed

--- routes/dataRouter.js
+++ routes/dataRouter.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 const { pool } = require('../config');
 /* GET home page. */
-router.get('/', function(req, res) {
-    query = 'SELECT * from ParkingLots';
+router.post('/', function(req, res) {
+    let { sensor_uuid, timestamp, status } = req.body;
+    console.log(sensor_uuid, timestamp, status);
+    if (sensor_uuid === undefined || timestamp === undefined || status === undefined) return res.status(400).json({error: 'Invalid Params'});
+    if (typeof status !== "boolean") return res.status(400).json({error: 'Expecting Boolean type in status field.'});
+
+    query = `INSERT INTO data VALUES ('${sensor_uuid}', '${timestamp}', ${status})`;
     pool.query(query, (err, results) => {
-        if (err) {
-            res.json({error: true});
-        }
-        console.log(results);
+        if (err) return res.json({error: true});
+        return res.status(200).json({result: 'OK'});
     })
 });
 
